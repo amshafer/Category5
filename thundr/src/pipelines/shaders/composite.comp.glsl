0e49d9134c1119138491e85316b7e9d5cdc62bc5
Austin Shafer <ashafer@badland.io>
2020-12-02 10:48:48 -0500

    WIP: correct the comp shader invocation

--- thundr/src/pipelines/shaders/composite.comp.glsl
+++ thundr/src/pipelines/shaders/composite.comp.glsl
@@ -3,8 +3,19 @@
 
 /* Compute implementation of a compositor */
 
+/*
+Specify the size of our working groups to be 256
+
+We will tile the display into blocks of 256 pixels whose composition
+will be handled by on workgroup. AMD recommends a wg size of 256, but
+it may be better to bump it up on nvidia??
+*/
+layout (local_size_x = 16, local_size_y = 16, local_size_z = 1 ) in;
+
+/* our render target: the swapchain frame to render into */
 layout(binding = 0, rgba32f) uniform image2D frame;
 
+/* the position/size/damage of our windows */
 layout(binding = 1) buffer windows
 {
     int width;
@@ -12,12 +23,13 @@ layout(binding = 1) buffer windows
 };
 
 void main() {
+    /* if this invocation extends past the resolution, then do nothing */
     if(gl_GlobalInvocationID.x >= width || gl_GlobalInvocationID.y >= height)
         return;
 
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
-            imageStore(frame, ivec2(j, i), vec4(0, 1, 0, 1));
-        }
-    }
+    /* get the position of this invocation in the framebuffer */
+    uint x = gl_GlobalInvocationID.x / width;
+    uint y = gl_GlobalInvocationID.y / height;
+
+    imageStore(frame, ivec2(x, y), vec4(0, 1, 0, 1));
 }
