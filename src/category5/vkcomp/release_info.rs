cd4c69353a7defa650c110515369f8dd8176f9a2
Austin Shafer <ashafer@badland.io>
2021-01-29 22:17:15 -0500

    Add unused generic release info

--- src/category5/vkcomp/release_info.rs
+++ src/category5/vkcomp/release_info.rs
@@ -42,3 +42,20 @@ impl fmt::Debug for DmabufReleaseInfo {
             .finish()
     }
 }
+
+pub struct GenericReleaseInfo {
+    // The wl_buffer that represents this  image
+    pub wl_buffer: wl_buffer::WlBuffer,
+}
+
+impl GenericReleaseInfo {
+    pub fn release(&mut self) {
+        self.wl_buffer.release();
+    }
+}
+
+impl Drop for GenericReleaseInfo {
+    fn drop(&mut self) {
+        self.release();
+    }
+}
