392d2498e0e11c3008ff34aabb79834d38f4ca7f
Austin Shafer <ashafer@badland.io>
2021-01-30 10:44:20 -0500

    Revert "Make SurfaceList an rc wrapped type"
    This reverts commit f2dd77c3312f882243154abcfc5463d239b81b54.

--- thundr/src/lib.rs
+++ thundr/src/lib.rs
@@ -312,7 +312,7 @@ impl Thundr {
             surfaces,
         );
         // Now that we have processed this surfacelist, unmark it as changed
-        surfaces.set_changed(false);
+        surfaces.l_changed = false;
         self.clear_damage_on_all_images();
     }
 
--- thundr/src/list.rs
+++ thundr/src/list.rs
@@ -5,73 +5,50 @@
 use super::surface::Surface;
 use std::iter::DoubleEndedIterator;
 
-use std::cell::RefCell;
-use std::rc::Rc;
-
-pub struct SurfaceListInternal {
+pub struct SurfaceList {
     /// This will get cleared during Thundr::draw
     pub(crate) l_changed: bool,
     l_vec: Vec<Surface>,
 }
 
-impl SurfaceListInternal {
-    pub fn iter(&self) -> impl DoubleEndedIterator<Item = &Surface> {
-        self.l_vec.iter()
-    }
-}
-
-pub struct SurfaceList {
-    pub(crate) sl_internal: Rc<RefCell<SurfaceListInternal>>,
-}
-
 impl SurfaceList {
     pub fn new() -> Self {
         Self {
-            sl_internal: Rc::new(RefCell::new(SurfaceListInternal {
-                l_changed: false,
-                l_vec: Vec::new(),
-            })),
+            l_changed: false,
+            l_vec: Vec::new(),
         }
     }
 
     pub fn remove_surface(&mut self, surf: Surface) {
-        let mut internal = self.sl_internal.borrow_mut();
-
-        let index = match internal.l_vec.iter().enumerate().find(|(_, s)| **s == surf) {
+        let index = match self.l_vec.iter().enumerate().find(|(_, s)| **s == surf) {
             Some((i, _)) => i,
             None => return,
         };
 
-        internal.l_changed = true;
-        internal.l_vec.remove(index);
+        self.l_changed = true;
+        self.l_vec.remove(index);
     }
 
     pub fn insert_surface_at(&mut self, surf: Surface, order: usize) {
-        let mut internal = self.sl_internal.borrow_mut();
-        internal.l_changed = true;
-        internal.l_vec.insert(order, surf);
+        self.l_changed = true;
+        self.l_vec.insert(order, surf);
     }
 
     pub fn push(&mut self, surf: Surface) {
-        let mut internal = self.sl_internal.borrow_mut();
-        internal.l_changed = true;
-        internal.l_vec.push(surf);
+        self.l_changed = true;
+        self.l_vec.push(surf);
     }
 
-    pub fn is_changed(&self) -> bool {
-        self.sl_internal.borrow().l_changed
-    }
-    pub fn set_changed(&self, changed: bool) {
-        self.sl_internal.borrow_mut().l_changed = changed;
+    pub fn iter(&self) -> impl DoubleEndedIterator<Item = &Surface> {
+        self.l_vec.iter()
     }
 
     pub fn clear(&mut self) {
-        let mut internal = self.sl_internal.borrow_mut();
-        internal.l_changed = true;
-        internal.l_vec.clear();
+        self.l_changed = true;
+        self.l_vec.clear();
     }
 
     pub fn len(&self) -> u32 {
-        self.sl_internal.borrow().l_vec.len() as u32
+        self.l_vec.len() as u32
     }
 }
