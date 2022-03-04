efde4b2174839157c84eb275e23e6e33ed48376a
Austin Shafer <ashafer@badland.io>
2022-03-04 12:02:31 -0500

    ways: Use our own ffi definition for devname_r
    This works around some nastiness in the libc crate

--- src/category5/ways/wl_drm.rs
+++ src/category5/ways/wl_drm.rs
@@ -19,6 +19,24 @@ use std::os::raw::{c_char, c_int};
 
 use super::protocol::wl_drm::wl_drm;
 
+// Alright now this gets gross
+//
+// Basically there's a big fight in libc about compatability between
+// different versions, i.e. the devparameter of devname_r may be
+// 32-bit on one freebsd version and 64 bit later. They still don't
+// have a way to fix this, and choose the lowest common denominator,
+// causing dev_t to be 32-bit in the libc crate, which is big wrong.
+//
+// https://github.com/rust-lang/libc/pull/2603#issuecomment-1003715259
+extern "C" {
+    pub fn devname_r(
+        #[cfg(target_arch = "x86_64")] dev: u64, // 64 bit on amd64
+        mode: libc::mode_t,
+        buf: *mut libc::c_char,
+        len: libc::c_int,
+    ) -> *mut libc::c_char;
+}
+
 /// In FreeBSD types.h:
 ///
 /// ```
@@ -30,7 +48,7 @@ use super::protocol::wl_drm::wl_drm;
 ///         ((dev_t)(_Minor & 0xff00) << 24) | (_Minor & 0xffff00ff));
 /// }
 /// ```
-fn makedev(major: u64, minor: u64) -> libc::dev_t {
+fn makedev(major: u64, minor: u64) -> u64 {
     (((major & 0xffffff00) as u64) << 32)
         | (((major & 0xff) as u64) << 8)
         | ((minor & 0xff00 as u64) << 24)
@@ -43,7 +61,7 @@ fn get_drm_dev_name(atmos: &Atmosphere) -> String {
     let mut dev_name = Vec::<c_char>::with_capacity(256); // Matching value of SPECNAMELEN in FreeBSD 13+
 
     let buf: *mut c_char = unsafe {
-        libc::devname_r(
+        devname_r(
             makedev(major as u64, minor as u64),
             SFlag::S_IFCHR.bits(), // Must be S_IFCHR or S_IFBLK
             dev_name.as_mut_ptr(),
@@ -59,7 +77,7 @@ fn get_drm_dev_name(atmos: &Atmosphere) -> String {
     let full_drm_name = format!("/dev/{}", cstr.to_string_lossy().into_owned());
     assert!(full_drm_name.starts_with("/dev/drm/"));
 
-    // Turn this in to /dev/dri/renderD128
+    // Turn this into /dev/dri/renderD128
     // 9 characters in /dev/drm/
     let drm_number_in_name = &full_drm_name[9..];
     format!("/dev/dri/renderD{}", drm_number_in_name)
