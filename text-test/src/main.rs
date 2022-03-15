42a6973fde2969ff67d2691863bf8da4549658be
Austin Shafer <ashafer@badland.io>
2022-03-15 17:30:08 -0600

    text: draw hello world text
    At this point we can handle larger messages with spacing and
    drawing.
    
    Tested text on linux

--- text-test/src/main.rs
+++ text-test/src/main.rs
@@ -12,10 +12,12 @@ use sdl2::{
 use std::collections::HashMap;
 
 struct Glyph {
-    /// The thundr image backing this glyph
-    g_image: th::Image,
-    g_width: usize,
-    g_height: usize,
+    /// The thundr image backing this glyph.
+    /// This will be none if the glyph does not have an outline
+    /// which happens if it's a space.
+    g_image: Option<th::Image>,
+    g_width: f32,
+    g_height: f32,
 }
 
 struct FontInstance<'a> {
@@ -33,20 +35,32 @@ impl<'a> FontInstance<'a> {
             .f_font
             .glyph_id(c)
             .with_scale_and_position(self.f_scale, ab::point(0.0, 0.0));
-        let outline = self.f_font.outline_glyph(ab_glyph).unwrap();
-        let bounds = outline.px_bounds();
+        let bounds = self.f_font.glyph_bounds(&ab_glyph);
+        let mut width = bounds.width();
+        let mut height = bounds.height();
 
-        let width = bounds.width() as usize;
-        let height = bounds.height() as usize;
+        // if there is no outline for a glyph, then it does not have any
+        // contents. In this case we just don't attach an image and record
+        // the bounds
+        let th_image = match self.f_font.outline_glyph(ab_glyph) {
+            Some(outline) => {
+                // Regrab the size, since we want the size of the glyph
+                // to use for a) the surface size, and b) the image size
+                let bounds = outline.px_bounds();
+                width = bounds.width();
+                height = bounds.height();
+                let mut img = vec![Pixel(0, 0, 0, 0); (width * height) as usize].into_boxed_slice();
 
-        let mut img = vec![Pixel(0, 0, 0, 0); width * height].into_boxed_slice();
+                outline.draw(|x, y, c| {
+                    img[(y * width as u32 + x) as usize] = Pixel(255, 255, 255, (c * 255.0) as u8)
+                });
 
-        outline.draw(|x, y, c| {
-            img[y as usize * width + x as usize] = Pixel(255, 255, 255, (c * 255.0) as u8)
-        });
-
-        let mimg = MemImage::new(img.as_ptr() as *mut u8, 4, width, height);
-        let th_image = thund.create_image_from_bits(&mimg, None).unwrap();
+                let mimg =
+                    MemImage::new(img.as_ptr() as *mut u8, 4, width as usize, height as usize);
+                Some(thund.create_image_from_bits(&mimg, None).unwrap())
+            }
+            None => None,
+        };
 
         // Create a new glyph for this UTF-8 character
         let glyph = Glyph {
@@ -58,16 +72,29 @@ impl<'a> FontInstance<'a> {
         self.f_glyphs.insert(c, glyph);
     }
 
+    fn ensure_glyph_exists(&mut self, thund: &mut Thundr, c: char) {
+        // If we have not imported this glyph, make it now
+        if !self.f_glyphs.contains_key(&c) {
+            self.create_glyph(thund, c);
+        }
+    }
+
+    fn get_glyph_bounds(&mut self, thund: &mut Thundr, c: char) -> (f32, f32) {
+        self.ensure_glyph_exists(thund, c);
+        let glyph = self
+            .f_glyphs
+            .get(&c)
+            .expect("Bug: glyph should have been created already");
+        (glyph.g_width, glyph.g_height)
+    }
+
     fn create_surface_for_char(
         &mut self,
         thund: &mut Thundr,
         c: char,
         pos: (f32, f32),
     ) -> th::Surface {
-        // If we have not imported this glyph, make it now
-        if !self.f_glyphs.contains_key(&c) {
-            self.create_glyph(thund, c);
-        }
+        self.ensure_glyph_exists(thund, c);
 
         let glyph = self
             .f_glyphs
@@ -75,7 +102,9 @@ impl<'a> FontInstance<'a> {
             .expect("Bug: glyph should have been created already");
         let mut surf =
             thund.create_surface(pos.0, pos.1, glyph.g_width as f32, glyph.g_height as f32);
-        thund.bind_image(&mut surf, glyph.g_image.clone());
+        if let Some(image) = glyph.g_image.as_ref() {
+            thund.bind_image(&mut surf, image.clone());
+        }
 
         return surf;
     }
@@ -86,13 +115,6 @@ impl<'a> FontInstance<'a> {
 struct Pixel(u8, u8, u8, u8);
 
 fn main() {
-    let mut inst = FontInstance {
-        f_font: ab::FontRef::try_from_slice(include_bytes!("../century_gothic.otf"))
-            .expect("Could not find font file"),
-        f_glyphs: HashMap::new(),
-        f_scale: 150.0,
-    };
-
     // SDL goodies
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -110,11 +132,26 @@ fn main() {
     let info = CreateInfo::builder().surface_type(surf_type).build();
     let mut thund = Thundr::new(&info).unwrap();
 
-    let bg_surf = inst.create_surface_for_char(&mut thund, 'G', (0.0, 0.0));
+    let mut inst = FontInstance {
+        f_font: ab::FontRef::try_from_slice(include_bytes!("../century_gothic.otf"))
+            .expect("Could not find font file"),
+        f_glyphs: HashMap::new(),
+        f_scale: 150.0,
+    };
+    let text = "Hello world.";
 
     // ----------- create list of surfaces
     let mut list = thundr::SurfaceList::new();
-    list.push(bg_surf.clone());
+    let mut offset = (0.0, 0.0);
+    let hor_spacing = inst.f_scale / 16.0;
+
+    for c in text.chars() {
+        let glyph_size = inst.get_glyph_bounds(&mut thund, c);
+        offset.1 = inst.f_scale - glyph_size.1;
+        let bg_surf = inst.create_surface_for_char(&mut thund, c, offset);
+        offset.0 += hor_spacing + glyph_size.0;
+        list.push(bg_surf.clone());
+    }
 
     // ----------- now wait for the app to exit
 
