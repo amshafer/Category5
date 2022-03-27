f3a2191cc306182e813cdc72cf2cf65ac6965431
Austin Shafer <ashafer@badland.io>
2022-03-27 17:53:45 -0400

    text-test: WIP add harfbuzz

--- text-test/src/main.rs
+++ text-test/src/main.rs
@@ -2,6 +2,7 @@ extern crate ab_glyph as ab;
 use ab::Font;
 extern crate thundr as th;
 use thundr::{CreateInfo, MemImage, SurfaceType, Thundr};
+extern crate rustybuzz as rb;
 
 extern crate sdl2;
 use sdl2::{
@@ -9,7 +10,8 @@ use sdl2::{
     keyboard::Keycode,
 };
 
-use std::collections::HashMap;
+use std::mem::MaybeUninit;
+use std::ptr::{addr_of, addr_of_mut};
 
 struct Glyph {
     /// The thundr image backing this glyph.
@@ -21,20 +23,60 @@ struct Glyph {
 }
 
 struct FontInstance<'a> {
+    /// The font reference for our rasterizer
     f_font: ab::FontRef<'a>,
-    /// Hashmap of glyphs to look up to find the thundr resources
-    f_glyphs: HashMap<char, Glyph>,
+    /// Font file raw contents. This is held for f_face.
+    f_data: Vec<u8>,
+    /// Our rustybuzz font face (see harfbuzz docs)
+    f_face: rb::Face<'a>,
+    /// Map of glyphs to look up to find the thundr resources
+    /// The ab::GlyphId is really just an index into this. That's all
+    /// glyph ids are, is the index of the glyph in the font.
+    f_glyphs: Vec<Glyph>,
     f_scale: f32,
 }
 
 impl<'a> FontInstance<'a> {
-    fn create_glyph(&mut self, thund: &mut Thundr, c: char) {
-        assert!(!self.f_glyphs.contains_key(&c));
+    fn new(font_path: &str, thund: &mut Thundr, scale: f32) -> Self {
+        let font_data = std::fs::read(font_path).unwrap();
 
-        let ab_glyph: ab::Glyph = self
-            .f_font
-            .glyph_id(c)
-            .with_scale_and_position(self.f_scale, ab::point(0.0, 0.0));
+        // See the uninit doc page
+        let mut inst = unsafe {
+            let mut inst = MaybeUninit::<FontInstance>::uninit();
+            let ptr = inst.as_mut_ptr();
+
+            // Using `write` instead of assignment via `=` to not call `drop` on the
+            // old, uninitialized value.
+            addr_of_mut!((*ptr).f_data).write(font_data);
+            // get a reference to f_data.
+            let data = &*addr_of!((*ptr).f_data);
+            // Now we can use the above reference to fill in the face and font
+            // entries in the struct. Here comes the self reference
+            addr_of_mut!((*ptr).f_font)
+                .write(ab::FontRef::try_from_slice(data).expect("Could not find font file"));
+            addr_of_mut!((*ptr).f_face).write(
+                rb::Face::from_slice(data, 0).expect("Could not initialize rustybuzz::Face"),
+            );
+            // Now initialize the boring correct ones
+            addr_of_mut!((*ptr).f_glyphs).write(Vec::new());
+            addr_of_mut!((*ptr).f_scale).write(scale);
+            // Finally tell the compiler it can go back to sane rules for
+            // borrow tracking.
+            inst.assume_init()
+        };
+
+        // Create a thundr surface for every glyph in this font
+        for i in 0..inst.f_font.glyph_count() {
+            assert!(inst.f_glyphs.len() == i);
+            let glyph = inst.create_glyph(thund, ab::GlyphId(i as u16));
+            inst.f_glyphs.push(glyph);
+        }
+
+        return inst;
+    }
+
+    fn create_glyph(&mut self, thund: &mut Thundr, id: ab::GlyphId) -> Glyph {
+        let ab_glyph: ab::Glyph = id.with_scale_and_position(self.f_scale, ab::point(0.0, 0.0));
         let bounds = self.f_font.glyph_bounds(&ab_glyph);
         let mut width = bounds.width();
         let mut height = bounds.height();
@@ -63,43 +105,26 @@ impl<'a> FontInstance<'a> {
         };
 
         // Create a new glyph for this UTF-8 character
-        let glyph = Glyph {
+        Glyph {
             g_image: th_image,
             g_width: width,
             g_height: height,
-        };
-
-        self.f_glyphs.insert(c, glyph);
-    }
-
-    fn ensure_glyph_exists(&mut self, thund: &mut Thundr, c: char) {
-        // If we have not imported this glyph, make it now
-        if !self.f_glyphs.contains_key(&c) {
-            self.create_glyph(thund, c);
         }
     }
 
-    fn get_glyph_bounds(&mut self, thund: &mut Thundr, c: char) -> (f32, f32) {
-        self.ensure_glyph_exists(thund, c);
-        let glyph = self
-            .f_glyphs
-            .get(&c)
-            .expect("Bug: glyph should have been created already");
+    #[allow(unused)]
+    fn get_glyph_bounds(&mut self, thund: &mut Thundr, id: ab::GlyphId) -> (f32, f32) {
+        let glyph = &self.f_glyphs[id.0 as usize];
         (glyph.g_width, glyph.g_height)
     }
 
     fn create_surface_for_char(
         &mut self,
         thund: &mut Thundr,
-        c: char,
+        id: ab::GlyphId,
         pos: (f32, f32),
     ) -> th::Surface {
-        self.ensure_glyph_exists(thund, c);
-
-        let glyph = self
-            .f_glyphs
-            .get(&c)
-            .expect("Bug: glyph should have been created already");
+        let glyph = &self.f_glyphs[id.0 as usize];
         let mut surf =
             thund.create_surface(pos.0, pos.1, glyph.g_width as f32, glyph.g_height as f32);
         if let Some(image) = glyph.g_image.as_ref() {
@@ -107,6 +132,41 @@ impl<'a> FontInstance<'a> {
         }
 
         return surf;
+    }
+
+    /// This is the big text drawing function
+    ///
+    /// Our Font instance is going to use the provided Thundr context to
+    /// create surfaces and lay them out. It's going to update the surface
+    /// list with them along the way.
+    fn layout_text(&mut self, thund: &mut Thundr, list: &mut th::SurfaceList, text: &str) {
+        // Set up our HarfBuzz buffers
+        let mut buffer = rb::UnicodeBuffer::new();
+        buffer.push_str(text);
+
+        // Now the big call to get the shaping information
+        let glyph_buffer = rb::shape(&self.f_face, Vec::with_capacity(0).as_slice(), buffer);
+        let infos = glyph_buffer.glyph_infos();
+        let positions = glyph_buffer.glyph_positions();
+
+        // This is how far we have advanced on a line
+        let mut cursor = (0.0, 0.0);
+
+        // for each UTF-8 code point in the string
+        for i in 0..glyph_buffer.len() {
+            let offset = (
+                cursor.0 + positions[i].x_offset as f32,
+                cursor.1 + positions[i].y_offset as f32,
+            );
+
+            let bg_surf =
+                self.create_surface_for_char(thund, ab::GlyphId(infos[i].glyph_id as u16), offset);
+            list.push(bg_surf.clone());
+
+            // Move the cursor
+            cursor.0 += positions[i].x_advance as f32;
+            cursor.1 += positions[i].y_advance as f32;
+        }
     }
 }
 
@@ -132,34 +192,13 @@ fn main() {
     let info = CreateInfo::builder().surface_type(surf_type).build();
     let mut thund = Thundr::new(&info).unwrap();
 
-    let mut inst = FontInstance {
-        f_font: ab::FontRef::try_from_slice(include_bytes!("../century_gothic.otf"))
-            .expect("Could not find font file"),
-        f_glyphs: HashMap::new(),
-        f_scale: 150.0,
-    };
-    let text = "Hello world. Testing larger messages that wrap";
+    let mut inst = FontInstance::new("./century_gothic.otf", &mut thund, 150.0);
+    let text = "Hello world. Testing ";
 
     // ----------- create list of surfaces
     let mut list = thundr::SurfaceList::new();
-    let mut offset = (0.0, 0.0);
-    let hor_spacing = inst.f_scale / 16.0;
-    let mut line_count = 1;
 
-    for c in text.chars() {
-        let glyph_size = inst.get_glyph_bounds(&mut thund, c);
-        offset.1 = (line_count as f32 * inst.f_scale) - glyph_size.1;
-        let bg_surf = inst.create_surface_for_char(&mut thund, c, offset);
-
-        if offset.0 >= 775.0 {
-            offset.0 = 0.0;
-            line_count += 1;
-        } else {
-            offset.0 += hor_spacing + glyph_size.0;
-        }
-
-        list.push(bg_surf.clone());
-    }
+    inst.layout_text(&mut thund, &mut list, text);
 
     // ----------- now wait for the app to exit
 
