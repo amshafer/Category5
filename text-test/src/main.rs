69314d87972d5304da4b4d46b7e93c0f20fe640d
Austin Shafer <ashafer@badland.io>
2022-04-21 01:06:21 -0400

    text: switch to lorem ipsum passage
    This is a larger text as an example of problems in rendering. It also
    makes the performance tank.

--- text-test/src/main.rs
+++ text-test/src/main.rs
@@ -225,11 +225,30 @@ fn main() {
     let info = CreateInfo::builder().surface_type(surf_type).build();
     let mut thund = Thundr::new(&info).unwrap();
 
-    let mut inst = FontInstance::new("./Ubuntu-Regular.ttf", &mut thund, 40.0);
-    let text = "This new version can actually render text in a fairly decent way.
-There are still some lingering weird points (such as extra spacing
-around certain vowels sometimes), but it can actually make strings
-that are pretty enough to not notice at first glance.";
+    let mut inst = FontInstance::new("./Ubuntu-Regular.ttf", &mut thund, 25.0);
+    let text = "But I must explain to you how all this mistaken idea of reprobating pleasure and
+extolling pain arose. To do so, I will give you a complete account of the system, and
+expound the actual teachings of the great explorer of the truth, the master-builder of
+human happiness. No one rejects, dislikes or avoids pleasure itself, because it is
+pleasure, but because those who do not know how to pursue pleasure rationally encounter
+consequences that are extremely painful. Nor again is there anyone who loves or pursues or
+desires to obtain pain of itself, because it is pain, but occasionally circumstances occur
+in which toil and pain can procure him some great pleasure. To take a trivial example,
+which of us ever undertakes laborious physical exercise, except to obtain some advantage
+from it? But who has any right to find fault with a man who chooses to enjoy a pleasure
+that has no annoying consequences, or one who avoids a pain that produces no resultant
+pleasure? [33] On the other hand, we denounce with righteous indignation and dislike
+men who are so beguiled and demoralized by the charms of pleasure of the moment, so
+blinded by desire, that they cannot foresee the pain and trouble that are bound to
+ensue; and equal blame belongs to those who fail in their duty through weakness of
+will, which is the same as saying through shrinking from toil and pain. These cases are
+perfectly simple and easy to distinguish. In a free hour, when our power of choice is
+untrammeled and when nothing prevents our being able to do what we like best, every
+pleasure is to be welcomed and every pain avoided. But in certain circumstances and
+owing to the claims of duty or the obligations of business it will frequently occur
+that pleasures have to be repudiated and annoyances accepted. The wise man therefore
+always holds in these matters to this principle of selection: he rejects pleasures to
+secure other greater pleasures, or else he endures pains to avoid worse pains.";
 
     // ----------- create list of surfaces
     let mut list = thundr::SurfaceList::new();
