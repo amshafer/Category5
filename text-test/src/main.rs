93c0fc200f994240d37824a5c3d926c6a32d8754
Austin Shafer <ashafer@badland.io>
2022-06-04 02:19:49 -0400

    text: Fix line wrapping at end of text block
    We weren't tracking if we need to do a clean line break at the last
    seen word separator, or if we should run to the ending index.

--- text-test/src/main.rs
+++ text-test/src/main.rs
@@ -178,8 +178,11 @@ impl<'a> FontInstance<'a> {
         infos: &[hb::GlyphInfo],
         positions: &[hb::GlyphPosition],
     ) {
-        let mut end_index = cursor.c_i;
-        let mut last_word = end_index + 1;
+        let mut end_index = cursor.c_i + 1;
+        // The last space separated point
+        let mut last_word = end_index;
+        // Should we use the above last word?
+        let mut line_wrap_needed = false;
         let mut line_pos = cursor.c_x;
 
         // First find the last glyph we should include on this line
@@ -208,12 +211,26 @@ impl<'a> FontInstance<'a> {
             // Check if we have exceeded the line width. if so, then this line ends
             // at the last known word break (last_word)
             if line_pos >= cursor.c_max {
+                line_wrap_needed = true;
                 break;
             }
         }
 
+        let end_of_line = if line_wrap_needed {
+            last_word
+        } else {
+            end_index
+        };
+
+        println!(
+            "Handling glyphs {}..{} out of {}",
+            cursor.c_i,
+            end_of_line,
+            infos.len()
+        );
+
         // Now do the above for real and commit it to the surface list
-        for i in cursor.c_i..last_word {
+        for i in cursor.c_i..end_of_line {
             // move to the next char
             cursor.c_i += 1;
 
@@ -310,8 +327,8 @@ fn main() {
     let info = CreateInfo::builder().surface_type(surf_type).build();
     let mut thund = Thundr::new(&info).unwrap();
 
-    let mut inst = FontInstance::new("./Ubuntu-Regular.ttf", thund.get_dpi() as u32, 9.0);
-    let text = "But I must explain to you how all this mistaken idea of reprobating pleasure and extolling pain arose. To do so, I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.  No one rejects, dislikes or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure? [33] On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammeled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.";
+    let mut inst = FontInstance::new("./Ubuntu-Regular.ttf", thund.get_dpi() as u32, 11.0);
+    let text = "But I must explain to you how all this mistaken idea of reprobating pleasure and extolling pain arose. To do so, I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.  No one rejects, dislikes or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure? [33] On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammeled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures,";
 
     // ----------- create list of surfaces
     let mut list = thundr::SurfaceList::new();
