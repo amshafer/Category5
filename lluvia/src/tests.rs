624b569710d8ff2c1bb47180a4d6571551724c92
Austin Shafer <ashafer@badland.io>
2022-09-10 10:34:31 -0400

    lluvia: Add drop detection for entity id in data test

--- lluvia/src/tests.rs
+++ lluvia/src/tests.rs
@@ -1,4 +1,6 @@
 use crate as ll;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 #[test]
 fn basic_test() {
@@ -24,19 +26,57 @@ fn basic_test() {
     assert_eq!(*data_ref, "Hola Lluvia");
 }
 
+struct TestData {
+    e: bool,
+    e1: bool,
+}
+struct Empty(&'static str, Rc<RefCell<TestData>>);
+
+impl Drop for Empty {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.0);
+        match self.0 {
+            "e" => self.1.borrow_mut().e = false,
+            "e1" => self.1.borrow_mut().e1 = false,
+            _ => panic!("Unrecognized string"),
+        }
+    }
+}
+
 // Test that we can add an Entity into a component table as data
+//
+// This is done by adding e in e1's data. We then add a custom struct
+// which will record if that element has been dropped yet in TestData
+// and test the values afterwards
 #[test]
 fn entity_in_component_data() {
     let mut inst = ll::Instance::new();
     let c = inst.add_component();
     let mut sesh = inst.open_session(c).unwrap();
+    let c1 = inst.add_component();
+    let mut sesh1 = inst.open_session(c1).unwrap();
 
+    let container = Rc::new(RefCell::new(TestData { e: true, e1: true }));
     {
-        let e = inst.add_entity();
-        let e_id = e.get_raw_id();
         let e1 = inst.add_entity();
-        sesh.set(&e1, e);
-        let data_ref = sesh.get(&e1).unwrap();
-        assert_eq!(data_ref.ecs_id, e_id);
+        sesh1.set(&e1, Empty("e1", container.clone()));
+
+        {
+            let e = inst.add_entity();
+            sesh1.set(&e, Empty("e", container.clone()));
+            let e_id = e.get_raw_id();
+
+            sesh.set(&e1, e);
+
+            let data_ref = sesh.get(&e1).unwrap();
+            assert_eq!(data_ref.ecs_id, e_id);
+        }
+
+        // Assert the data is still valid
+        let data = container.borrow();
+        assert!(data.e && data.e1);
     }
+    // Assert the data is not valid since we dropped e1
+    let data = container.borrow();
+    assert!(!data.e && !data.e1);
 }
