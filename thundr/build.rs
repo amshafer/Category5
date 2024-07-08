f1cf29a2133eea6c3d288bd7abf7bb84fd8c550f
Austin Shafer <ashafer@badland.io>
2024-07-07 23:50:08 -0400

    thundr: add search path for FreeBSD

---
+++ thundr/build.rs
@@ -0,0 +1,7 @@
+// Build helper for thundr
+
+fn main() {
+    // Search /usr/local/lib/ for libvulkan
+    // This is needed on FreeBSD as ash doesn't do this by default
+    println!(r"cargo::rustc-link-search=/usr/local/lib/");
+}
